5e13b855f930fd9dabf1809affb4948df95246da
JaredLederman <jaredlederman@dhcp-vl2041-27300.eduroam.cornell.edu>
2026-03-16 08:54:36 -0400

    Revert hero headline to original

--- app/LandingClient.tsx
+++ app/LandingClient.tsx
@@ -119,7 +119,7 @@ export default function LandingClient() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: 'easeOut', delay: 0.35 }}
         >
-          Your Personal<br /><em>Wealth Management.</em>
+          You can&apos;t fix<br /><em>what you can&apos;t see.</em>
         </motion.h1>
 
         <motion.div
